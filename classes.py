2a0dd692959c42969ff92844ebbb44b2943ef52e
BenjWolf <53669230+BenjWolf@users.noreply.github.com>
2019-08-19 09:42:08 -0500

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 class Card:
+    # rarity 1: common, 2: uncommon, 3: rare, 4: one-of-a-kind
     def __init__(self, cardID, interactID, cardName, rarity):
         self.cardID = cardID
         self.interactID = interactID
@@ -13,15 +14,18 @@ class Item:
 
 
 class Location:
-    def __init__(self, address, originalType, area, levelName, description):
+    # originalType 1: chest card, 2: hidden card, 3: item chest, 4: item reward
+    def __init__(self, address, originalType, area, levelName, description, typeAddress=None):
         self.address = address
         self.originalType = originalType
         self.area = area
         self.levelName = levelName
         self.description = description
+        self.typeAddress = typeAddress
 
 
 class Summon:
+    # attackType 04: medium, 05: heavy
     def __init__(self, address, attackType):
         self.address = address
         self.attackType = attackType
